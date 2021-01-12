72b56515f296988f84f1ea28fb997ebe8d39d5e4
atsuhiro-narita <alayashiki@gmail.com>
2021-01-12 11:53:28 +0900

    add both order

--- cryt/command/get.py
+++ cryt/command/get.py
@@ -9,19 +9,30 @@ class Get(object):
         self.__client = client
 
     def order(self):
-        print(self.__client.get_orders())
+        table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("Type")
+        table.add_column("Symbol")
+        table.add_column("Side")
+        table.add_column("Price")
+        table.add_column("Size")
+
+        for order in self.__client.get_orders():
+            table.add_row(order['type'], order['symbol'], str(order['side']), str(order['price']), str(order['remaining']))
+        console = Console()
+        console.print(table)
 
     def position(self):
         positions = self.__client.get_positions()
 
         print(positions)
         table = Table(show_header=True, header_style="bold magenta")
+        table.add_column("Symbol")
         table.add_column("Side")
         table.add_column("Price")
         table.add_column("Size")
         table.add_column("Pnl")
         for position in positions:
-            table.add_row(position['side'], str(position['price']), str(position['size']), str(position['pnl']))
+            table.add_row(position['product_code'], position['side'], str(position['price']), str(position['size']), str(position['pnl']))
         console = Console()
         console.print(table)
 
--- cryt/command/order.py
+++ cryt/command/order.py
@@ -19,3 +19,10 @@ class _LimitOrder(object):
     def buy(self, size: float, price_depth: float):
         price = self.__client.get_ticker()["bid"] - price_depth
         print(self.__client.create_limit_order("buy", price, size))
+
+    def both(self, size: float, price_depth: float):
+        ticker = self.__client.get_ticker()
+        buy_price = ticker["bid"] - price_depth
+        sell_price = ticker["ask"] + price_depth
+        print(self.__client.create_limit_order("buy", buy_price, size))
+        print(self.__client.create_limit_order("sell", sell_price, size))
